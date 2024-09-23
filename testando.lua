39768203e3bf42f00b4654c2f91a926674cec798
REDzHUB <138633493+REDzHUB@users.noreply.github.com>
2024-09-23 08:42:10 -0300

    Update testando.lua

--- testando.lua
+++ testando.lua
@@ -1,11 +1,23 @@
+local Settings = ...
+
+local _ENV = (getgenv or getrenv or getfenv)() or _G
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TeleportService = game:GetService("TeleportService")
+local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
-local CommF = Remotes:WaitForChild("CommF")
+local Validator = Remotes:WaitForChild("Validator")
+local CommF = Remotes:WaitForChild("CommF_")
 
-local Enemies = workspace:WaitForChild("Enemy")
+local Enemies = workspace:WaitForChild("Enemies")
+
+local RenderStepped = RunService.RenderStepped
+local Heartbeat = RunService.Heartbeat
+local Stepped = RunService.Stepped
+
+local Player = Players.LocalPlayer
 
 local sethiddenproperty = sethiddenproperty or (function(...) return ... end)
 local setupvalue = setupvalue or (debug and debug.setupvalue)
@@ -24,9 +36,16 @@ local function GetEnemyName(string)
 end
 
 local Module = {} do
+  local CachedBaseParts = {}
   local CachedEnemies = {}
+  local CachedChars = {}
   local Items = {}
   
+  local placeId = game.PlaceId
+  
+  Module.Sea = (placeId == 2753915549 and 1) or (placeId == 4442272183 and 2) or (placeId == 7449423635 and 3) or 0
+  
+  Module.SpawnedFruits = {}
   Module.EnemyPosition = {}
   Module.FruitsId = {
     ["rbxassetid://15060012861"] = "Rocket-Rocket",
@@ -69,8 +88,52 @@ local Module = {} do
     ["https://assetdelivery.roblox.com/v1/asset/?id=10537896371"] = "Dragon-Dragon"
   }
   
+  function EnableBuso()
+    local Char = Player.Character
+    if Settings.AutoBuso and Module:IsAlive(Char) and not Char:FindFirstChild("HasBuso") then
+      Module:FireRemote("Buso")
+    end
+  end
+  
+
+  
+  local function GetBaseParts(Char)
+    if CachedBaseParts[Char] then
+      return CachedBaseParts[Char]
+    end
+    
+    local baseParts = {}
+    
+    for _,part in Char:GetDescendants() do
+      if part:IsA("BasePart") then
+        table.insert(baseParts, part)
+      end
+    end
+    
+    CachedBaseParts[Char] = baseParts
+    return baseParts
+  end
+  
   function Module.newCachedEnemy(Name, Enemy)
     CachedEnemies[Name] = Enemy
+  end
+  
+  function Module:FireRemote(...)
+    return CommF:InvokeServer(...)
+  end
+  
+  function Module:IsAlive(Char)
+    if CachedChars[Char] then
+      return CachedChars[Char].Health > 0
+    end
+    
+    local Hum = Char and Char:FindFirstChildOfClass("Humanoid")
+    CachedChars[Char] = Hum
+    return Hum and Hum.Health > 0
+  end
+  
+  function Module:IsFruit(Part)
+    return (Part.Name == "Fruit " or Part:GetAttribute("OriginalName")) and Part:FindFirstChild("Handle")
   end
   
   function Module:Rejoin()
@@ -92,37 +155,51 @@ local Module = {} do
           task.spawn(function()
             ReplicatedStorage.__ServerBrowser:InvokeServer("teleport", id)
           end)
-				end
+        end
       end
     end
   end
   
-  function Module:IsAlive(Char)
-    local Hum = Char and Char:FindFirstChild("Humanoid")
-    return Hum and Hum.Health > 0
-  end
-  
   function Module:GetEnemy(Name)
-    return self.SpawnedEnemy[Name]
+    return self.EnemySpawned[Name]
   end
   
   function Module:GetAliveEnemy(Name)
     local CachedEnemy = CachedEnemies[Name]
-    if CachedEnemy and self:IsAlive(CachedEnemy) then
+    if CachedEnemy and self.IsAlive(CachedEnemy) then
       return CachedEnemy
     end
     
     for _,Enemy in next, Enemies:GetChildren() do
-      if Enemy.Name == Name and self:IsAlive(Enemy) then
+      if Enemy.Name == Name and self.IsAlive(Enemy) then
         self.newCachedEnemy(Name, Enemy)
         return Enemy
       end
     end
     for _,Enemy in next, ReplicatedStorage:GetChildren() do
-      if Enemy.Name == Name and self:IsAlive(Enemy) then
+      if Enemy.Name == Name and self.IsAlive(Enemy) then
         self.newCachedEnemy(Name, Enemy)
         return Enemy
       end
+    end
+    
+    return nil
+  end
+  
+  function Module:BringEnemies(ToEnemy)
+    if Settings.BringMobs and self.IsAlive(ToEnemy) then
+      local Target = ToEnemy:GetPivot()
+      for _,Enemy in ipairs(Enemies:GetChildren()) do
+        if Enemy.Name == ToEnemy.Name and self.IsAlive(Enemy) and Enemy.PrimaryPart then
+          if Enemy ~= ToEnemy and (Enemy.PrimaryPart.Position - Target.p).Magnitude < Settings.BringDistance then
+            Enemy:PivotTo(Target)
+          end
+          self.HitBox(Enemy)
+        end
+      end
+      pcall(sethiddenproperty, Player, "SimulationRadius",  m_huge)
+    elseif self.IsAlive(ToEnemy) then
+      self.HitBox(ToEnemy)
     end
   end
   
@@ -132,8 +209,8 @@ local Module = {} do
       if Enemy then
         rawset(self, index, Enemy)
         Enemy.Humanoid.Died:Once(function() rawset(self, index, nil) end)
-        return Enemy
       end
+      return Enemy
     end
   })
   
@@ -216,20 +293,64 @@ local Module = {} do
           return nil
         end
         
-        local Tool = Player.Backpack:FindFirstChild(Name)
-        if Tool then
-          self.Equipped = Tool
-          Char:WaitForChild("Humanoid"):EquipTool(Tool)
+        if Name then
+          local Tool = Player.Backpack:FindFirstChild(Name)
+          if Tool then
+            self.Equipped = Tool
+            Char:WaitForChild("Humanoid"):EquipTool(Tool)
+          end
+        else
+          local Tip = Settings.FarmTool
+          for _,Tool in Player.Backpack:GetChildren() do
+            if Tool:IsA("Tool") and Tool.ToolTip == FarmTool then
+              self.Equipped = Tool
+              Char:WaitForChild("Humanoid"):EquipTool(Tool)
+            end
+          end
         end
       end
     end
   })
   
+  Module.HitBox = setmetatable({}, {
+    __call = function(self, Enemy)
+      if not self[Enemy] then
+        rawset(self, Enemy, true)
+        Enemy.PrimaryPart.CanCollide = false
+        Enemy.PrimaryPart.Size = Vector3.new(50, 50, 50)
+        Enemy.Humanoid.WalkSpeed = 0
+        Enemy.Humanoid:ChangeState(14)
+      end
+    end
+  })
+  
+  task.spawn(function()
+    local Fruits = Module.SpawnedFruits
+    
+    workspace.ChildAdded:Connect(function(Part)
+      if Module:IsFruit(Part) then
+        table.insert(Fruits, Part)
+        Part:GetPropertyChangedSignal("Parent"):Once(function()
+          table.remove(Fruits, table.find(Fruits, Part))
+        end)
+      end
+    end)
+    
+    for _,Part in next, workspace:GetChildren() do
+      if Module:IsFruit(Part) then
+        table.insert(Fruits, Part)
+        Part:GetPropertyChangedSignal("Parent"):Once(function()
+          table.remove(Fruits, table.find(Fruits, Part))
+        end)
+      end
+    end
+  end)
+  
   task.spawn(function()
     local EnemySpawns = WaitChilds(workspace, "_WorldOrigin", "EnemySpawns")
     local locations = Module.EnemyLocations
     
-    table.foreach(EnemySpawns:GetChildren(), locations)
+    table.foreach(EnemySpawns:GetChildren(), function(_,...) locations(...) end)
   end)
   
   task.spawn(function()
@@ -263,4 +384,72 @@ local Module = {} do
       end
     })
   end)
+  
+  task.spawn(function()
+    local DeathM = require(WaitChilds(ReplicatedStorage, "Effect", "Container", "Death"))
+    local CameraShaker = require(WaitChilds(ReplicatedStorage, "Util", "CameraShaker"))
+    
+    CameraShaker:Stop()
+    if hookfunction then hookfunction(DeathM, function()end) end
+  end)
+  
+  Module.TweenBlock = (function()
+    if _ENV.TweenBlock then
+      return _ENV.TweenBlock
+    end
+    
+    local block = Instance.new("Part")
+    block.Size = Vector3.new(1, 1, 1)
+    block.Name = tostring(Player.UserId) .. "_Block"
+    block.Anchored = true
+    block.CanCollide = false
+    block.CanTouch = false
+    block.CanQuery = false
+    block.Transparency = 1
+    
+    local velocity = Instance.new("BodyVelocity")
+    velocity.Name = "BV_Player"
+    velocity.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
+    velocity.Velocity = Vector3.new()
+    
+    local steppped = nil
+    local heartbeat = nil
+    
+    steppped = Stepped:Connect(function()
+      local Char = Player.Character
+      if _ENV.OnFarm and velocity.Parent == Char then
+        for _,Part in GetBaseParts(Char) do
+          if Part.CanCollide then
+            Part.CanCollide = false
+          end
+        end
+      end
+    end)
+    
+    heartbeat = Heartbeat:Connect(function()
+      if not block or not velocity then
+        return heartbeat:Disconnect(), steppped:Disconnect()
+      end
+      
+      local Char = Player.Character
+      if _ENV.OnFarm and Module:IsAlive(Char) then
+        local plrPP = Char.PrimaryPart
+        if plrPP then
+          if (plrPP.Position - block.Position).Magnitude < 150 then
+            plrPP.CFrame = block.CFrame
+          else
+            block.CFrame = plrPP.CFrame
+          end
+          if velocity.Parent ~= plrPP then
+            velocity.Parent = plrPP
+          end
+        end
+      elseif velocity.Parent then
+        velocity.Parent = nil
+      end
+    end)
+    
+    _ENV.TweenBlock = block
+    return block
+  end)()
 end
